9235f27c62071283c3155f193e16a68a46c411a5
murata-naoya <murata.small.baseball@gmail.com>
2021-06-30 13:59:48 +0000

    親コンポーネントから子コンポーネントへ

--- try-component/main.js
+++ try-component/main.js
@@ -1,10 +1,19 @@
-Vue.component('list-items',{
-  template: '<ul><li>{{num1}}</li><li>{{num2}}</li><li>{{num3}}</li></ul>',
-  data: function(){
-    return {num1: 5, num2: 2, num3: 8}
-  }
+Vue.component('list-item', {
+  props: {
+    listItems: {
+      type: Object,
+      required: true
+    }
+  },
+  template: '<li>{{listItems.name}}</li>'
 })
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    listItems: [
+      {name: 'apple'},
+      {name: 'orange'}
+    ]
+  }
 })
