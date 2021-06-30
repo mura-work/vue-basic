89f0f6c0eebab4d1004f4fa204f1f886db50608a
murata-naoya <murata.small.baseball@gmail.com>
2021-06-30 11:49:38 +0000

    コンストラクタベースの定義

--- try-component/main.js
+++ try-component/main.js
@@ -1,7 +1,18 @@
-Vue.component('list-item', {
-  template: '<p>lesson</p>'
+Vue.component('fruits-list-title', {
+  template: '<h1>フルーツ一覧</h1>'
 })
 
-var app = new Vue({
-  el: '#app',
+Vue.component('fruits-list', {
+  template: '<div><fruits-list-title></fruits-list-title></div>'
 })
+
+var FruitsListsTitle = Vue.extend({
+  template: '<li>aaa</li>'
+})
+
+
+new Vue({
+  el: '#app'
+})
+
+new FruitsListsTitle().$mount('fruits-list')
