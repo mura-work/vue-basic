b48155d4ec1320351802f239f8bb21ebd2ad0437
murata <murata.small.baseball@gmail.com>
2021-07-01 07:35:34 +0900

    v-once v-pre

--- try-component/main.js
+++ try-component/main.js
@@ -1,33 +1,11 @@
-
-var counterButton = Vue.extend({
-  template: '<span>{{item.count}}個<button v-on:click="addToCart">追加</button></span>',
-  methods: {
-    addToCart: function(){
-      this.counter += 1;
-      this.$emit('increments')
-    }
-  },
-})
-
-new Vue({
+var app = new Vue({
   el: '#app',
-  components: {
-    'counter-button': counterButton
-  },
   data: {
-    total: 0,
-    listItems: [
-      {name: 'apple', count: 0},
-      {name: 'banana', count: 0},
-      {name: 'orange', count: 0},
-    ]
+    message: 'message'
   },
   methods: {
-    incrementCartStatus: function(){
-      this.total += 1
-    },
-    clearCount: function(){
-      this.total = 0
+    clickHandler: function(event){
+      this.message = this.message.split('').reverse().join('')
     }
   }
 })
