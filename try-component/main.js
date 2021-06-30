a9271339ab539cac5f93a268fac6b685eb06f6ae
murata-naoya <murata.small.baseball@gmail.com>
2021-06-30 14:47:57 +0000

    子コンポーネントから親コンポーネントへ

--- try-component/main.js
+++ try-component/main.js
@@ -1,19 +1,33 @@
-Vue.component('list-item', {
-  props: {
-    listItems: {
-      type: Object,
-      required: true
+
+var counterButton = Vue.extend({
+  template: '<span>{{item.count}}個<button v-on:click="addToCart">追加</button></span>',
+  methods: {
+    addToCart: function(){
+      this.counter += 1;
+      this.$emit('increments')
     }
   },
-  template: '<li>{{listItems.name}}</li>'
 })
 
 new Vue({
   el: '#app',
+  components: {
+    'counter-button': counterButton
+  },
   data: {
+    total: 0,
     listItems: [
-      {name: 'apple'},
-      {name: 'orange'}
+      {name: 'apple', count: 0},
+      {name: 'banana', count: 0},
+      {name: 'orange', count: 0},
     ]
+  },
+  methods: {
+    incrementCartStatus: function(){
+      this.total += 1
+    },
+    clearCount: function(){
+      this.total = 0
+    }
   }
 })
